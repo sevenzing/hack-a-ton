38487f5f03c0f9efa072cb9dc4738119974b6f4d
steph-mipt <siniakov.se@phystech.edu>
2022-07-02 17:33:57 +0300

    tg wrapper added

---
+++ tg-bot/main.py
@@ -0,0 +1,13 @@
+"""
+    Telegram bot - CV downloader
+    :date: 02.07.2022
+    :author: Steph
+"""
+
+from bot import my_bot
+
+def main():
+    my_bot.infinity_polling()
+
+if __name__ == '__main__':
+    main()
