4bd5c170eb8d9e55406dda702b231783ae4dabf6
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-07-02 15:05:45 +0300

    fix deploy

--- paymnent-server/blockchain.js
+++ paymnent-server/blockchain.js
@@ -1,19 +1,26 @@
-import { SEED_BASE64, PUBLIC_RPC_URL, API_KEY } from "./config.js"
-
-import TonWeb from "tonweb";
-
+const TonWeb = require("tonweb");
+const { SEED_BASE64, PUBLIC_RPC_URL, API_KEY } = require("./config");
 const BN = TonWeb.utils.BN;
 const toNano = TonWeb.utils.toNano;
 
 
-export const tonweb = new TonWeb(new TonWeb.HttpProvider(PUBLIC_RPC_URL, {API_KEY})); // Initialize TON SDK
-export const key_pair = tonweb.utils.keyPairFromSeed(TonWeb.utils.base64ToBytes(SEED_BASE64))
-export const wallet = tonweb.wallet.create({
-    publicKey: key_pair.publicKey
-});
-export const wallet_address = await wallet.getAddress();
+let tonweb, key_pair, wallet, wallet_address;
+const init = async () => {
 
-export const deploy_contract = async (client_public_key, initial_balance) => {
+    const apiKey = API_KEY; 
+    tonweb = new TonWeb(new TonWeb.HttpProvider(PUBLIC_RPC_URL, {apiKey}));
+
+    key_pair = tonweb.utils.keyPairFromSeed(TonWeb.utils.base64ToBytes(SEED_BASE64))
+    wallet = tonweb.wallet.create({
+        publicKey: key_pair.publicKey
+    });
+    wallet_address = await wallet.getAddress();
+}
+init()
+
+
+
+const deploy_contract = async (client_public_key, initial_balance, channel_id = Math.floor(Math.random() * 10000000)) => {
     const client_wallet = tonweb.wallet.create({
         publicKey: client_public_key
     })
@@ -21,20 +28,21 @@ export const deploy_contract = async (client_public_key, initial_balance) => {
     
     const state = get_initial_state(initial_balance)
     const channelConfig = {
-        channelId: new BN(Math.floor(Math.random() * 10000000)),
+        channelId: new BN(channel_id),
         addressA: wallet_address,
         addressB: client_address,
         initBalanceA: state.balanceA,
         initBalanceB: state.balanceB
     }
     console.log('deploy contact: channel config =', channelConfig)
-
     const channel = tonweb.payments.createChannel({
         ...channelConfig,
         isA: true,
         myKeyPair: key_pair,
         hisPublicKey: client_public_key,
     })
+    const channelAddress = await channel.getAddress();
+    console.log("channel address", channelAddress.toString(true, true, true))
     const fromWallet = channel.fromWallet({
         wallet: wallet,
         secretKey: key_pair.secretKey
@@ -42,14 +50,22 @@ export const deploy_contract = async (client_public_key, initial_balance) => {
     console.log('start deploy')
     const tx = await fromWallet.deploy().send(toNano('0.05'));
     console.log('deploy tx =', tx)
-    return tx
+    return (tx, channel)
 }
+
+
+
 
 const get_initial_state = (initial_balance) => {
     return {
         balanceA: toNano('0'), // A's initial balance in Toncoins. Next A will need to make a top-up for this amount
-        balanceB: toNano(new BN(initial_balance)), // B's initial balance in Toncoins. Next B will need to make a top-up for this amount
+        balanceB: toNano(initial_balance + ''), // B's initial balance in Toncoins. Next B will need to make a top-up for this amount
         seqnoA: new BN(0), // initially 0
         seqnoB: new BN(0)  // initially 0
     };
+}
+
+module.exports = {
+    tonweb, key_pair, wallet, wallet_address,
+    deploy_contract, toNano, BN
 }
--- paymnent-server/main.js
+++ paymnent-server/main.js
@@ -1,8 +1,29 @@
-import TonWeb from "tonweb";
+const TonWeb = require("tonweb");
 
-import {deploy_contract, tonweb} from "./blockchain.js"
+const {deploy_contract, tonweb, toNano, BN} = require("./blockchain");
+const { sleep } = require("./utils");
 
 
-let client_public_key = tonweb.utils.keyPairFromSeed(TonWeb.utils.base64ToBytes('YmJvYnVzYWpzZGFpa2RucWlkc25hbHdkaWFqd29kYXc=')).publicKey;
+let client_key = tonweb.utils.keyPairFromSeed(TonWeb.utils.base64ToBytes('YmJvYnVzYWpzZGFpa2RucWlkc25hbHdkaWFqd29kYXc='))
+let client_public_key = client_key.publicKey;
+let client_wallet = tonweb.wallet.create({publicKey: client_public_key});
 
-await deploy_contract(client_public_key, 1)
+
+const main = async () => {
+    const INITIAL = 1
+    const INITIAL_BN = new BN(toNano(INITIAL + ''))
+    let tx, channel = await deploy_contract(client_public_key, INITIAL, 71823).catch((reason) => {console.log("DEPLOY ERROR:", reason)})
+    await sleep(10000)
+    console.log(await channel.getChannelState())
+
+
+    
+    tx = await channel.fromWallet({
+            wallet: client_wallet,
+            secretKey: client_key.secretKey
+        })
+        .topUp({coinsA: new BN(0), coinsB: INITIAL_BN})
+        .send(INITIAL_BN.add(toNano('0.05')))
+    console.log(tx)
+}
+main()
