4b42c13de50100156e348c6e89f301babd21880c
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-07-03 13:40:48 +0300

    fix uuid

--- paymnent-server/server.js
+++ paymnent-server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const uuid = require("UUID")
+const uuid = require("uuid")
 const tonMnemonic = require("tonweb-mnemonic")
 const config = require('./config')
 const TonWeb = require("tonweb");
